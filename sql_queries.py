b1a1e4766167f2b98ef406461bd64750d7f83549
natbernard <natbernard15@gmail.com>
2023-02-18 20:55:02 +0300

    commit

--- sql_queries.py
+++ sql_queries.py
@@ -7,35 +7,82 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF TABLE EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF TABLE EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF TABLE EXISTS songplays;"
+user_table_drop = "DROP TABLE IF TABLE EXISTS users;"
+song_table_drop = "DROP TABLE IF TABLE EXISTS songs;"
+artist_table_drop = "DROP TABLE IF TABLE EXISTS artists;"
+time_table_drop = "DROP TABLE IF TABLE EXISTS time;"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS staging_events(
+    
+)
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs(
+
+)
 """)
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs(
+    songplay_id varchar
+    start_time bigint
+    user_id text
+    level text
+    song_id text
+    artist_id text
+    session_id int
+    location text
+    user_agent text
+)
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users(
+    user_id int
+    first_name text
+    last_name text
+    gender text
+    level text
+)
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs(
+    song_id text
+    title text
+    artist_id text
+    year int
+    duration int
+)
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs(
+    artist_id text
+    name text
+    location text
+    latitude numeric
+    longitude numeric
+)
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs(
+    start_time timestamp
+    hour int
+    day int
+    week int
+    month int
+    year int
+    weekday int
+)
 """)
 
 # STAGING TABLES
